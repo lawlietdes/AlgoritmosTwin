616afc237704562080c7a8fbe77fa5f0bb1cd4f6
Luis <luisprogram23@gmail.com>
2020-11-13 12:08:21 -0600

    Subo mi nuevo entorno de trabajo java 13 11 2020

---
+++ AlgorithmsTrainning/src/Greedy/Bulbs.java
@@ -0,0 +1,27 @@
+package Greedy;
+
+import java.util.ArrayList;
+
+public class Bulbs {
+	public static void main (String args[]) {
+		ArrayList<Integer> A = new ArrayList<Integer>();
+		
+		A.add(0);
+		A.add(1);
+		A.add(0);
+		A.add(1);
+		System.out.println(bulbs(A));
+	}
+	
+	public static int bulbs(ArrayList<Integer> A) {
+		int minimumswitches = 0;
+		for (int i = 0; i < A.size(); i++) {
+			int turned = A.get(i) + minimumswitches;
+			if(turned % 2 == 0) {
+				minimumswitches++;
+			}
+		}
+		
+		return minimumswitches;
+    }
+}
